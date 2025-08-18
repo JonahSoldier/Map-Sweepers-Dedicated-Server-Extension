c5d9a3d334d873fadf68ae1045be34f235a507d7
JonahSoldier <86782614+JonahSoldier@users.noreply.github.com>
2025-08-18 01:46:48 +0100

    Votekick & vote-end
    Basics for the kick-player and end-round votes. Currently unrefined.

---
+++ lua/autorun/client/cl_init.lua
@@ -0,0 +1,4 @@
+if not(engine.ActiveGamemode() == "mapsweepers") then return end
+include("jcms_serverExt/shared.lua")
+
+--TODO: Tab autocomplete
---
+++ lua/jcms_serverExt/shared.lua
@@ -0,0 +1,11 @@
+AddCSLuaFile()
+
+jcms = jcms or {}
+
+local FCVAR_JCMS_NOTIFY_AND_SAVE = bit.bor(FCVAR_ARCHIVE, FCVAR_NOTIFY)
+local FCVAR_JCMS_SHARED_SAVED = bit.bor(FCVAR_REPLICATED, FCVAR_JCMS_NOTIFY_AND_SAVE)
+
+jcms.cvar_votekickThreshold = CreateConVar("jcms_votekick_threshold", "0.75", FCVAR_JCMS_SHARED_SAVED, "The ratio of players needed for a vote kick. 0.75 means 75% of the server (rounded up).", 0, 1)
+jcms.cvar_voteEndRoundThreshold = CreateConVar("jcms_voteEndRound_threshold", "0.5", FCVAR_JCMS_SHARED_SAVED, "The ratio of players needed for an end-round vote to be successful. 0.5 means 50% (rounded up).", 0, 1)
+
+jcms.cvar_votekickTime = CreateConVar("jcms_votekick_duration", "30", FCVAR_JCMS_SHARED_SAVED, "The time to ban vote-kicked players for (0 is permanent!)")
